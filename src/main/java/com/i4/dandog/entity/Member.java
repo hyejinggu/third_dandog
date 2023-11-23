9c63d9f88b23ce2cdae09ec1d70f0853f365b648
User -2023CHGYS\Administrator <bmhsr5@naver.com>
2023-11-23 14:54:13 +0900

    member test

---
+++ src/main/java/com/i4/dandog/entity/Member.java
@@ -0,0 +1,27 @@
+package com.i4.dandog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int item_no;
+	
+}
