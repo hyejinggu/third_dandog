507f8ba3f84021a8dac7b50b845601b82ef28315
namuri23 <rrang2305@gmail.com>
2023-11-23 19:16:14 +0900

    파일이름바꾸기 Order 를 ItemOrder 로 수정

---
+++ src/main/java/com/i4/dandog/entity/ItemOrder.java
@@ -0,0 +1,37 @@
+package com.i4.dandog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemOrder extends BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int order_num;
+	private String user_id;
+	private int item_no;
+	private int total_price;
+	private String payment;
+	private String shipping_msp;
+	private String recipient_name;
+	private String recipient_phone;
+	private int post_code;
+	private String user_address1;
+	private String user_address2;
+	
+}
