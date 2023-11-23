4027422d310e42f00a8dab98be2c9d320c132bf0
namuri23 <rrang2305@gmail.com>
2023-11-23 17:29:19 +0900

    config패키지 생성, 내부 java파일 2개 추가

---
+++ src/main/java/com/i4/dandog/config/DandogConfig.java
@@ -0,0 +1,18 @@
+package com.i4.dandog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+//=> 스프링에서 설정파일로 인식하도록 함
+public class DandogConfig {
+// => 일반적인 Bean 설정용
+	
+	@Bean
+	public PasswordEncoder getPasswordEncord() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
