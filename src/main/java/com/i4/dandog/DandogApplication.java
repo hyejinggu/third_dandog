55517aa2101f7a29cc63336356edfcce60b63e9f
User -2023CHGYS\Administrator <bmhsr5@naver.com>
2023-11-23 13:03:15 +0900

    test

---
+++ src/main/java/com/i4/dandog/DandogApplication.java
@@ -0,0 +1,13 @@
+package com.i4.dandog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DandogApplication.class, args);
+	}
+
+}
