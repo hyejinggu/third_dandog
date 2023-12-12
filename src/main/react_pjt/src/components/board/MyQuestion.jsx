b00871270968c457d54fbff7fa05bca1aff9ed3b
namuri23 <rrang2305@gmail.com>
2023-12-12 20:05:59 +0900

    qna 글순서정렬, 버튼css 수정

--- src/main/react_pjt/src/components/board/MyQuestion.jsx
+++ src/main/react_pjt/src/components/board/MyQuestion.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const MyQuestionsComponent = () => {
     const [myQuestions, setMyQuestions] = useState([]);
-    const [selectedRows, setSelectedRows] = useState([]);
-    const [selectAll, setSelectAll] = useState(false);
+    // const [selectedRows, setSelectedRows] = useState([]);
+    // const [selectAll, setSelectAll] = useState(false);
     const loginId = sessionStorage.getItem("loginId");
 
     useEffect(() => {
@@ -23,46 +23,46 @@ const MyQuestionsComponent = () => {
     }, [loginId]); // loginId가 변경될 때마다 useEffect가 호출
 
     // 체크박스
-    const handleCheckboxChange = (qna_seq) => {
-        // 해당 행의 체크 여부를 토글
-        setSelectedRows((prevSelectedRows) => {
-            if (prevSelectedRows.includes(qna_seq)) {
-                return prevSelectedRows.filter((row) => row !== qna_seq);
-            } else {
-                return [...prevSelectedRows, qna_seq];
-            }
-        });
-    };
+    // const handleCheckboxChange = (qna_seq) => {
+    //     // 해당 행의 체크 여부를 토글
+    //     setSelectedRows((prevSelectedRows) => {
+    //         if (prevSelectedRows.includes(qna_seq)) {
+    //             return prevSelectedRows.filter((row) => row !== qna_seq);
+    //         } else {
+    //             return [...prevSelectedRows, qna_seq];
+    //         }
+    //     });
+    // };
 
-    const handleSelectAllChange = () => {
-        // 전체 선택/해제 상태를 토글하고 선택된 행을 업데이트
-        setSelectAll((prevSelectAll) => !prevSelectAll);
-        setSelectedRows((prevSelectedRows) => {
-            if (!selectAll) {
-                // 전체 선택이었을 때는 모든 행을 추가
-                return myQuestions.map((qna) => qna.qna_seq);
-            } else {
-                // 전체 선택 해제일 때는 빈 배열
-                return [];
-            }
-        });
-    };
+    // const handleSelectAllChange = () => {
+    //     // 전체 선택/해제 상태를 토글하고 선택된 행을 업데이트
+    //     setSelectAll((prevSelectAll) => !prevSelectAll);
+    //     setSelectedRows((prevSelectedRows) => {
+    //         if (!selectAll) {
+    //             // 전체 선택이었을 때는 모든 행을 추가
+    //             return myQuestions.map((qna) => qna.qna_seq);
+    //         } else {
+    //             // 전체 선택 해제일 때는 빈 배열
+    //             return [];
+    //         }
+    //     });
+    // };
 
-    const handleDeleteSelected = async () => {
-        // 선택된 행을 삭제하는 요청 등 추가적인 로직을 구현할 수 있음
-        try {
-            // 예시: 선택된 행의 qna_seq를 이용하여 삭제 요청
-            await axios.post('/qnar/deleteSelected', { selectedRows });
-            // 삭제 후 질문 목록을 업데이트
-            const response = await axios.get(`/qnar/myQuestions/${loginId}`);
-            setMyQuestions(response.data);
-            // 선택된 행 초기화
-            setSelectedRows([]);
-            setSelectAll(false);
-        } catch (error) {
-            console.error('Error deleting selected questions:', error);
-        }
-    };
+    // const handleDeleteSelected = async () => {
+    //     // 선택된 행을 삭제하는 요청 등 추가적인 로직을 구현할 수 있음
+    //     try {
+    //         // 예시: 선택된 행의 qna_seq를 이용하여 삭제 요청
+    //         await axios.post('/qnar/deleteSelected', { selectedRows });
+    //         // 삭제 후 질문 목록을 업데이트
+    //         const response = await axios.get(`/qnar/myQuestions/${loginId}`);
+    //         setMyQuestions(response.data);
+    //         // 선택된 행 초기화
+    //         setSelectedRows([]);
+    //         setSelectAll(false);
+    //     } catch (error) {
+    //         console.error('Error deleting selected questions:', error);
+    //     }
+    // };
 
 
 
@@ -72,7 +72,7 @@ const MyQuestionsComponent = () => {
             <table border="1">
                 <thead>
                     <tr>
-                        <th></th>
+                        {/* <th></th> */}
                         {/* <th>Seq</th> */}
                         {/* <th>ID</th> */}
                         {/* <th>Category</th> */}
@@ -86,13 +86,13 @@ const MyQuestionsComponent = () => {
                 <tbody>
                     {myQuestions.map((qna) => (
                         <tr key={qna.qna_seq}>
-                            <td>
+                            {/* <td>
                                 <input
                                     type="checkbox"
                                     checked={selectedRows.includes(qna.qna_seq)}
                                     onChange={() => handleCheckboxChange(qna.qna_seq)}
                                 />
-                            </td>
+                            </td> */}
                             {/* <td>{qna.qna_seq}</td> */}
                             {/* <td>{qna.user_id}</td> */}
                             {/* <td>{qna.qna_category}</td> */}
@@ -106,12 +106,12 @@ const MyQuestionsComponent = () => {
                 
             </table>
             &nbsp;
-            <input
+            {/* <input
                 type="checkbox"
                 checked={selectAll}
                 onChange={handleSelectAllChange}
-            />
-            &nbsp;<button onClick={handleDeleteSelected}>삭제</button>
+            /> */}
+            {/* &nbsp;<button onClick={handleDeleteSelected}>삭제</button> */}
             
         </div>
     );
