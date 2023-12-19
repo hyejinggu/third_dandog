5a2478e33ff19ad07de80bfc82d61c59342f5feb
namuri23 <rrang2305@gmail.com>
2023-12-19 17:08:02 +0900

    Notice 공지사항 생성, 수정

--- src/main/java/com/i4/dandog/service/NoticeService.java
+++ src/main/java/com/i4/dandog/service/NoticeService.java
@@ -1,5 +1,33 @@
 package com.i4.dandog.service;
 
-public class NoticeService {
+import java.util.List;
+
+import com.i4.dandog.entity.Faq;
+import com.i4.dandog.entity.Notice;
+
+public interface NoticeService {
+	
+	
+	public List<Notice> selectsearchAllList(String searchText);
+	
+	public List<Notice> selectsearchList(String selectedNotice_category, String searchText);
+
+	public List<Notice> selectList();
+	
+	public List<Notice> findByCategory(String notice_category);
+	
+	public Notice selectOne(int notice_seq);
+
+	public int save(Notice entity);
+	
+	public int ndelete(int notice_seq);
+	
+	public List<Notice> findByloginId(String loginid);
+	
+	public List<Notice> findAllDesc();
+
+	
+	
 
 }
+
